4556ec010e7ee72562d0890de661699aa5dd8fe3
Arsen339 <arsensubbotin@yandex.ru>
2021-05-21 21:13:52 +0300

    Losing added

--- main.py
+++ main.py
@@ -10,6 +10,12 @@ screen_height=700
 #Скорость игрока
 global player_speed
 player_speed=10
+#Скорость врагов
+global enemy_speed
+enemy_speed=10
+
+
+
 #максимальное количество злаков
 global max_food
 max_food=20
@@ -18,6 +24,12 @@ max_food=20
 global foods
 foods=[turtle.Turtle()]*(max_food+1)
 
+global max_dangers
+max_dangers=20
+
+global dangers
+dangers=[turtle.Turtle()]*(max_dangers+1)
+
 
 #Таблички с надписями
 btn1 = turtle.Turtle()
@@ -25,8 +37,12 @@ btn2= turtle.Turtle()
 btn2.clear()
 btn2.hideturtle()
 btn2.penup()
-btn2.goto(0,230)
-btn2.write("Красный-опасная зона", font=("Arial", 12, "normal"))
+btn2.goto(-30,270)
+btn2.write("Берегись Квадратиков!Ешь желтые злаки!", font=("Arial", 12, "normal"))
+btn3 = turtle.Turtle()
+
+
+
 
 #Перечень цветов
 colors=['green','maroon1','purple1','lime green','blue4','light coral']
@@ -43,9 +59,10 @@ class FoodAm():
 food_stat=FoodAm(0)
 
 #Установим размер окна 
-wndow = turtle.Screen()
-wndow.title("Screen")
-wndow.setup(screen_length,screen_height)
+global window
+window = turtle.Screen()
+window.title("Screen")
+window.setup(screen_length,screen_height)
 
 #Объект игрока
 player=turtle.Turtle()
@@ -220,9 +237,6 @@ def Eaten():
             write_mes=('Score: ',stat.count)
             btn1.write(write_mes, font=("Arial", 12, "normal"))
             food_stat.food_now=food_stat.food_now-1
-            
-            
-
             AddFood()
             
 
@@ -238,9 +252,67 @@ def AddFood():
             foods[i].shape("triangle")
             foods[i].goto(randint(-150,150),randint(-150,150))
 
+#Создадим врагов
 def MakeEnemy():
+    for i in range(5):
+        dangers[i]=turtle.Turtle()
+        dangers[i].penup()
+        dangers[i].goto(-200,randint(-180,180))
+        dangers[i].speed(100)
+        dangers[i].color("red","black")
+        dangers[i].shape("square")
+        dangers[i].pensize(5)
+        dangers[i].pencolor('red')
+    for i in range(5,10):
+        dangers[i]=turtle.Turtle()
+        dangers[i].penup()
+        dangers[i].speed(100)
+        dangers[i].color("black","red")
+        dangers[i].shape("square")
+        dangers[i].goto(200,randint(-180,180))
+    for i in range(10,15):
+        dangers[i]=turtle.Turtle()
+        dangers[i].penup()
+        dangers[i].speed(100)
+        dangers[i].color("black","red2")
+        dangers[i].shape("square")
+        dangers[i].goto(randint(-180,180),-200)
+    for i in range(15,20):
+        dangers[i]=turtle.Turtle()
+        dangers[i].penup()
+        dangers[i].speed(100)
+        dangers[i].color("black","grey26")
+        dangers[i].shape("square")
+        dangers[i].goto(randint(-180,180),200)
 
+#Передвижение врага
+def MoveEnemy():
+    for i in range(max_dangers):
+        if (i>=0) and (i<5):
+            x=dangers[i].xcor()+enemy_speed+randint(0,0)
+            dangers[i].setx(x)
+        if (i>=5) and (i<10):
+            x=dangers[i].xcor()-enemy_speed+randint(0,10)
+            dangers[i].setx(x)
+        if (i>=10) and(i<15):
+            y=dangers[i].ycor()+enemy_speed+randint(0,15)
+            dangers[i].sety(y)
+        if (i>=15) and(i<20):
+            y=dangers[i].ycor()-enemy_speed+randint(0,15)
+            dangers[i].sety(y)
 
+def Losing():
+    for i in range(max_dangers):
+        if abs(dangers[i].xcor()-player.xcor())<10 and abs(dangers[i].ycor()-player.ycor())<10:
+        
+            btn3.clear()
+            btn3.hideturtle()
+            btn3.penup()
+            btn3.goto(0,0)
+            turtle.bye()
+            
+            btn3.write("Game Over", font=("Arial", 22, "normal"))
+            turtle.exitonclick()
 
 
 
@@ -250,23 +322,28 @@ def up():
     player.sety(y)
     player.setheading(90)
     Eaten()
+    Losing()
+
 def down():
     y = player.ycor() - player_speed
     player.sety(y)
     player.setheading(-90)
     Eaten()
+    Losing()
     
 def left():
     x = player.xcor() - player_speed
     player.setx(x)
     player.setheading(180)
     Eaten()
+    Losing()
 
 def right():
     x = player.xcor() + player_speed
     player.setx(x)
     player.setheading(0)
     Eaten()
+    Losing()
 
 
 
@@ -287,13 +364,15 @@ for i in range(10):
     DrawStar(5,144,30,'red',choice(colors),randint(-screen_length/2, screen_length/2),randint(-screen_length/2.5, screen_length/2.5))
 for i in range(10):
     DrawStar(8,135,30,'red',choice(colors),randint(-screen_length/2, screen_length/2),randint(-screen_length/2.5, screen_length/2.5))
-DrawSquare(80,"red",'red',-40,-120)
+
+
 
 
 
 
 #Добавим еды
 MakeFood()
+MakeEnemy()
 
 #считывание нажатий мыши
 turtle.listen()
@@ -301,8 +380,10 @@ turtle.onkeypress(up, 'Up')
 turtle.onkeypress(down, 'Down')
 turtle.onkeypress(left, 'Left')
 turtle.onkeypress(right, 'Right')
-
-
+turtle.onkey(MoveEnemy, 'Up')
+turtle.onkey(MoveEnemy, 'Down')
+turtle.onkey(MoveEnemy, 'Left')
+turtle.onkey(MoveEnemy, 'Right')
 
 #Завершение
 turtle.done()
